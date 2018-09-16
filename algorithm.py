9e65cb12afdba37ebca709484167daf0702df8c9
Aaditya Singh <awesomeaaditya@gmail.com>
2018-09-15 23:29:37 -0400

    small changes.

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,13 @@
 # The aim of this file is to take the phoneme_audios pickled data and run our algorithm on it
 
+import random from random
+
 phoneme_audios = {} # TODO this should be read in somehow
+# The structure of this data structure is phoneme to list of audios (each of which is a list)
 
 num_guesses = 100
+
+for phoneme in phoneme_audios:
+	audios = phoneme_audios[phoneme]
+	for i in range(num_guesses):
+		clips = random.sample(audios, 2)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pickle as pkl
 
+# We read in 
+
 print("This program lets you make NAME HERE pronounce anything!")
 
 again = 'y'
@@ -7,6 +9,10 @@ again = 'y'
 while (again.lower() == 'y'):
 	input_text = input("Enter text you want to generate speech for: ")
 
-
+	generate_audio_out(input_text)
 
 	again = input("Audio generated! Generate more? (Y/N): ")
+
+# For this method, we want to split the input text
+def generate_audio_out(input_text):
+	pass
