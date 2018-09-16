064662065efdc8d425851b9e88bc7e2da94511d6
Aaditya Singh <awesomeaaditya@gmail.com>
2018-09-16 04:43:55 -0400

    Debugged algo part 1.

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,18 @@
 # The aim of this file is to take the phoneme_audios pickled data and run our algorithm on it
 
+import random
 import numpy as np
 
-phoneme_audios = {"test": "test"} # TODO this should be read in somehow
+phoneme_audios = {"test": np.array([list(range(10)), [1, 2, -1, 3, 4, -2, 1]])} # TODO this should be read in somehow
 # The structure of this data structure is phoneme to list of audios (each of which is a list)
+
+def evaluate(candidate_field, phoneme):
+	audios = phoneme_audios[phoneme]
+	retval = 0
+	for audio in audios:
+		retval += np.max(np.correlate(candidate_field, audio, "full"))
+	print(retval)
+	return retval
 
 num_guesses = 1
 min_length = 2
@@ -14,16 +23,16 @@ for phoneme in phoneme_audios:
 	fields = []
 	field_evals = []
 	for i in range(num_guesses):
-		# indices = np.random.randint(audios.shape[0], 2)
-		first = np.array([1, 2, -1, 3, 4, -2, 1]) #audios[indices[0]]
-		second = np.array(list(range(10))) #audios[indices[1]]
+		indices = random.sample(range(audios.shape[0]), 2)
+		first = audios[indices[0]]
+		second = audios[indices[1]]
 		print(np.correlate(first, second, "full"))
 		position = np.argmax(np.correlate(first, second, "full"))
 
-		first_start = max(0, position-second.size+1)
-		first_end = min(position, first.size-1) - min_length + 1
-		second_start = max(0, second.size-1 - position)
-		second_end = second.size-1 - max((position-first.size+1),  0) - min_length+1
+		first_start = max(0, position-len(second)+1)
+		first_end = min(position, len(first)-1) - min_length + 1
+		second_start = max(0, len(second)-1 - position)
+		second_end = len(second)-1 - max((position-len(first)+1),  0) - min_length+1
 		print(first_start, first_end, second_start, second_end)
 		print((first_end-first_start) == (second_end-second_start)) # Should be true always
 		num_positions = first_end-first_start+1
@@ -46,12 +55,11 @@ for phoneme in phoneme_audios:
 		end = run_ends[temp_ind]
 		first_field = first[first_start+start:first_start+end-1+min_length]
 		second_field = second[second_start+start:second_start+end-1+min_length]
-		receptive_field = (first_field+second_field)/2
+		print(first_field, second_field)
+		receptive_field = np.add(first_field, second_field)/2
 
 		fields.append(receptive_field)
-		field_evals.appennd(evaluate(receptive_field, phoneme))
-
-def evaluate(candidate_field, phoneme):
-	audios = phoneme_audios[phoneme]
+		evaluate(receptive_field, phoneme)
+		field_evals.append(evaluate(receptive_field, phoneme))
 		
 
