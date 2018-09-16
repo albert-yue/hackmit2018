dbbb5fa7ef6ba9f4825fd59636560a02da729171
Aaditya Singh <awesomeaaditya@gmail.com>
2018-09-15 23:00:19 -0400

    Added some code for main and algorithm.

---
+++ algorithm.py
@@ -0,0 +1,5 @@
+# The aim of this file is to take the phoneme_audios pickled data and run our algorithm on it
+
+phoneme_audios = {} # TODO this should be read in somehow
+
+num_guesses = 100
---
+++ main.py
@@ -0,0 +1,12 @@
+import pickle as pkl
+
+print("This program lets you make NAME HERE pronounce anything!")
+
+again = 'y'
+
+while (again.lower() == 'y'):
+	input_text = input("Enter text you want to generate speech for: ")
+
+
+
+	again = input("Audio generated! Generate more? (Y/N): ")
